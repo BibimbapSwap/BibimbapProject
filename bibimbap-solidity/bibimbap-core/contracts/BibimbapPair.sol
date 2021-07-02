da2e65b5c2eb0d3d50448187978b8457643211a0
BibimbapSwap <bibimbapswap@gmail.com>
2021-07-02 16:30:03 +0900

    commit periphery

--- bibimbap-solidity/bibimbap-core/contracts/BibimbapPair.sol
+++ bibimbap-solidity/bibimbap-core/contracts/BibimbapPair.sol
@@ -169,7 +169,7 @@ contract BibimbapPair is IBibimbapPair, BibimbapERC20 {
         require(to != _token0 && to != _token1, 'Bibimbap: INVALID_TO');
         if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
         if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
-        if (data.length > 0) IBibimbapCallee(to).BibimbapCall(msg.sender, amount0Out, amount1Out, data);
+        if (data.length > 0) IBibimbapCallee(to).bibimbapCall(msg.sender, amount0Out, amount1Out, data);
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
         }
